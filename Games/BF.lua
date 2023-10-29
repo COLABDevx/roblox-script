df2aa212b26794afb1d158682c735859edf7d8f7
colabevx <colabx.2001@gmail.com>
2023-10-29 18:20:41 +0700

    update BF.lua and Loader.lua

--- Games/BF.lua
+++ Games/BF.lua
@@ -1,12 +1,25 @@
 local LocalPlayer = game.Players.LocalPlayer
+local TweenService = game:GetService("TweenService")
+local Character = LocalPlayer.Character
+local HumanoidRootPart = Character.HumanoidRootPart
+local RunService = game:GetService("RunService")
+local UserInputService = game:GetService("UserInputService")
 
 
 local Configs = {
+    ["Islands"] = {
+        ["Starter Prirate"] = {
+            ["NickName"] = "Windmill",
+            ["Sea"] = 1,
+            ["CFrame"] = CFrame.new(1110.41284, 16.2993984, 1450.15393, 0.626955211, -5.22833687e-08, -0.779055297, -1.25271375e-08, 1, -7.71926238e-08, 0.779055297, 5.81556492e-08, 0.626955211)
+        }
+    },
+
     ["Enemies"] = {
         ["Bandit"] = {
             ["Level"] = 5,
             ["QuestLevel"] = 0,
-            ["IsLand"] = "Starter Prirate"
+            ["IsLand"] = "Starter Prirate",
         }
     },
     ["NPCS"] = {
@@ -16,15 +29,62 @@ local Configs = {
             ["IsQuest"] = true,
             ["QuestEnemyType"] = "prirate",
             ["Island"] = "Starter Prirate",
+            ["CFrame"] = CFrame.new(1059.37195, 15.4495068, 1550.4231, 0.939700544, -0, -0.341998369, 0, 1, -0, 0.341998369, 0, 0.939700544)
         }
     },
     ["Quests"] = {
+        ["_IsAttack"] = false,
+        ["_Attack_Emnemy"] = nil,
+        ["_Remotes"] = {
+            ["GetQuest"] = function(args)
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(
+                args))
+            end,
+            ["AbandonQuest"] = function()
+                local args = { [1] = "AbandonQuest" }
+                game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("CommF_"):InvokeServer(unpack(
+                args))
+            end
+        },
         ["Bandits"] = {
-            ["NPC"] = "Bandit",
+            ["EnemyName"] = "Bandit",
+            ["NPC"] = "Bandit Quest Giver",
             ["Level"] = 0,
+            ["QuestRemoteArgs"] = {
+                [1] = "StartQuest",
+                [2] = "BanditQuest1",
+                [3] = 1
+            }
         }
     }
 }
+
+local Trades = {}
+
+local Tween = {
+    ["Current"] = nil,
+    ["ToCFrame"] = function(CFrame)
+        local speed = 250
+
+        local distance = (CFrame.Position - HumanoidRootPart.Position).Magnitude
+        local time = distance / speed
+
+        local tweenInfo = TweenInfo.new(time, Enum.EasingStyle.Linear)
+
+        return TweenService:Create(HumanoidRootPart, tweenInfo, {
+            CFrame = CFrame
+        })
+    end,
+    ["StopTween"] = function()
+        if Tween.Current ~= nil then
+            Tween.Current.Cancel()
+        end
+    end
+}
+
+-- print(setclipboard(tostring(HumanoidRootPart.CFrame)))
+
+-- Configs["Quests"]["_Remotes"].AbandonQuest()
 
 function GetPlayerData(Type)
     if Type == "Level" then
@@ -33,34 +93,125 @@ function GetPlayerData(Type)
 end
 
 function GetCurrentPlayerQuestNpc(PlayerLevel)
-    for _,npc in pairs(Configs["NPCS"]) do
+    for _, npc in pairs(Configs["NPCS"]) do
         if PlayerLevel >= npc.Level then
-            return _,npc
+            return npc
         end
     end
 
     return nil
 end
 
-function GetCurrentPlayerQuest(PlayerLevel)
-    -- local GetNPC = Configs["NPCS"][""]
-    local Name,Data = GetCurrentPlayerQuestNpc(PlayerLevel)
+function GetCurrentPlayerNPCQuest(PlayerLevel)
+    local Data = GetCurrentPlayerQuestNpc(PlayerLevel)
 
-    local NPC_HumanoidRootPart = workspace.NPCs[Name].HumanoidRootPart
+    -- local NPC = workspace.NPCs[Name]
 
-    print(NPC_HumanoidRootPart.CFrame)
+    return Data
+end
 
-    -- print(workspace["NPCs"]:GetChildren()[Data])
+function FindNearEenemy(Name)
+    local nearestEnemy = nil
+    local minDistance = math.huge -- Initialize to a very large number
+
+    -- print(Name)
+
+    for _, enemy in pairs(workspace.Enemies:GetChildren()) do
+        if enemy.Name == Name then
+            -- print(Name)
+            
+
+            local enemyRootPart = enemy:FindFirstChild("HumanoidRootPart")
+            -- print(enemyRootPart)
+            enemyRootPart.Size = Vector3.new(70,70,70)
+            enemyRootPart.CanCollide = false
+
+            if enemyRootPart then
+                local distance = (enemyRootPart.CFrame.Position - HumanoidRootPart.Position).Magnitude
+                if distance < minDistance then
+                    minDistance = distance
+                    nearestEnemy = enemy
+                end
+            end
+        end
+    end
+
+    return nearestEnemy
 end
 
 -- local PlayerLevel = GetPlayerData("Level")
-GetCurrentPlayerQuest(1)
+local Data = GetCurrentPlayerNPCQuest(1)
 
+HumanoidRootPart.Anchored = false
+Tween.Current = Tween.ToCFrame(Data["CFrame"])
+Tween.Current:Play()
+Tween.Current.Completed:Connect(function()
+    local QuestRemoteArgs = Configs["Quests"]["Bandits"]["QuestRemoteArgs"]
+    Configs["Quests"]["_Remotes"].GetQuest(QuestRemoteArgs)
+
+    local target = FindNearEenemy(Configs["Quests"]["Bandits"]["EnemyName"])
+    Configs.Quests._Attack_Emnemy = target
+    
+    if target ~= nil then
+        Tween.Current = Tween.ToCFrame(target.HumanoidRootPart.CFrame * CFrame.new(Vector3.new(0, 15, 0)))
+        Tween.Current:Play()
+
+        Tween.Current.Completed:Connect(function ()
+            Configs["Quests"]["_IsAttack"] = true
+            -- HumanoidRootPart.Anchored = true
+        end)
+    end
+end)
 
 local function WalkOnWater(SizeY)
     local WaterBasePlane = workspace.Map["WaterBase-Plane"]
-    -- print(workspace.Map["WaterBase-Plane"].Size.Y)
-    workspace.Map["WaterBase-Plane"].Size = Vector3.new(workspace.Map["WaterBase-Plane"].Size.X,SizeY,workspace.Map["WaterBase-Plane"].Size.Z)
+    workspace.Map["WaterBase-Plane"].Size = Vector3.new(WaterBasePlane.Size.X, SizeY, WaterBasePlane.Size.Z)
 end
 
 WalkOnWater(113)
+
+
+
+Trades.Main = RunService.Stepped:Connect(function ()
+    if(Configs["Quests"]["_IsAttack"]) then
+        -- HumanoidRootPart.CFrame = Configs["Quests"]["_Attack_Emnemy"].HumanoidRootPart.CFrame * CFrame.new(Vector3.new(0,40,0))
+    end
+
+    -- print(workspace.Enemies[Configs["Quests"]["_Attack_Emnemy"]])
+
+    if Configs["Quests"]["_Attack_Emnemy"] == nil then
+        -- print("die")
+    end
+end)
+
+local KeyPress = {
+    LeftControl = false,
+    R = false,
+}
+
+UserInputService.InputBegan:Connect(function(input, _gameProcessed)
+    if input.KeyCode  == Enum.KeyCode.LeftControl then
+        KeyPress.LeftControl = true
+    end
+    if input.KeyCode == Enum.KeyCode.R then
+        KeyPress.R = true
+    end
+
+    if KeyPress.R and KeyPress.LeftControl then
+       for _, t in pairs(Trades) do
+            t:Disconnect()
+       end
+       Trades = {}
+    end
+
+end)
+
+UserInputService.InputEnded:Connect(function(input, _gameProcessed)
+    if input.KeyCode  == Enum.KeyCode.LeftControl then
+        KeyPress.LeftControl = false
+    end
+
+    if input.KeyCode == Enum.KeyCode.R then
+        KeyPress.R = false
+    end
+end)

6f6386a2e4324a54b7795c6f3ec2409a2abe84c1
colabevx <colabx.2001@gmail.com>
2023-10-29 13:28:38 +0700

    update BF.lua

--- Games/BF.lua
+++ Games/BF.lua
@@ -1,11 +1,41 @@
-_.G['ColabX'].Configs = {
+local LocalPlayer = game.Players.LocalPlayer
 
-}
-
-local function WalkOnWater()
-    local WaterBasePlane = workspace.Map["WaterBase-Plane"]
-
-    WaterBasePlane.Size = WaterBasePlane.Size + Vecter3.new(0,13,0)
+local function GetPlayerData(Type)
+    if Type = "Level" then
+        print(LocalPlayer.Data)
+    end
 end
 
-WalkOnWater()
+GetPlayerData("Level")
+
+local Config = {
+    ["Enemies"] = {
+        ["Bandit"] = {
+            ["Level"] = 5,
+            ["QuestLevel"] = 0,
+            ["IsLand"] = "Starter Prirate"
+        }
+    },
+    ["NPCS"] = {
+        ["Bandit Quest Giver"] = {
+            ["Sea"] = 1,
+            ["IsQuest"] = true,
+            ["QuestEnemyType"] = "prirate",
+            ["Island"] = "Starter Prirate",
+        }
+    },
+    ["Quests"] = {
+        ["Bandits"] = {
+            ["NPC"] = "Bandit",
+            ["minLevel"] = 0,
+        }
+    }
+}
+
+local function WalkOnWater(SizeY)
+    local WaterBasePlane = workspace.Map["WaterBase-Plane"]
+    -- print(workspace.Map["WaterBase-Plane"].Size.Y)
+    workspace.Map["WaterBase-Plane"].Size = Vector3.new(workspace.Map["WaterBase-Plane"].Size.X,SizeY,workspace.Map["WaterBase-Plane"].Size.Z)
+end
+
+WalkOnWater(113)
